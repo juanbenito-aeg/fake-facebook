404585ba41e95ce60ac98f69a6c8e04726413cbf
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-19 10:18:27 +0200

    feat: create a tab bar to switch between different screens

--- App.tsx
+++ App.tsx
@@ -7,6 +7,11 @@ import Groups from "./screens/Groups";
 import Profile from "./screens/Profile";
 import Notifications from "./screens/Notifications";
 import Menu from "./screens/Menu";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
@@ -16,6 +21,51 @@ const App = () => {
       <ScrollView>
         <Header />
       </ScrollView>
+
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => {
+            return {
+              tabBarShowLabel: false,
+              tabBarShowIcon: true,
+              tabBarActiveTintColor: "#3a86e9",
+              tabBarInactiveTintColor: "#9f9f9f",
+              tabBarIcon: ({ color }) => {
+                let iconName;
+
+                if (route.name === "Home") {
+                  iconName = "home";
+                } else if (route.name === "Friends") {
+                  iconName = "account-multiple-outline";
+                } else if (route.name === "Groups") {
+                  iconName = "account-group";
+                } else if (route.name === "Profile") {
+                  iconName = "account-circle-outline";
+                } else if (route.name === "Notifications") {
+                  iconName = "bell-outline";
+                } else if (route.name === "Menu") {
+                  iconName = "menu";
+                }
+
+                return (
+                  <MaterialCommunityIcons
+                    name={iconName}
+                    size={26}
+                    color={color}
+                  />
+                );
+              },
+            };
+          }}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Friends" component={Friends} />
+          <Tab.Screen name="Groups" component={Groups} />
+          <Tab.Screen name="Profile" component={Profile} />
+          <Tab.Screen name="Notifications" component={Notifications} />
+          <Tab.Screen name="Menu" component={Menu} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </>
   );
 };
