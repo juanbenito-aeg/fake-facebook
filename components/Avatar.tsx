2261e6b385f2e7fc37076b0fc1a2e23c18161466
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-21 19:19:28 +0200

    feat: adapt the Avatar.tsx component so that it is properly rendered when used in the stories section

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components/native";
 interface AvatarProps {
   source: string;
   online: boolean;
+  story: string;
+  checked: boolean;
 }
 
 const Container = styled.View`
@@ -15,6 +17,8 @@ const ProfileImage = styled.Image`
   width: 40px;
   height: 40px;
   border-radius: 20px;
+  border-width: ${(props) => (props.story ? "3px" : 0)};
+  border-color: ${(props) => (props.checked ? "#ffffff" : "#1777f2")};
 `;
 
 const UserActive = styled.View`
@@ -28,10 +32,10 @@ const UserActive = styled.View`
   background-color: #10d24b;
 `;
 
-const Avatar = ({ source, online }: AvatarProps) => {
+const Avatar = ({ source, online, story, checked }: AvatarProps) => {
   return (
     <Container>
-      <ProfileImage source={source} />
+      <ProfileImage source={source} story={story} checked={checked} />
       {online && <UserActive />}
     </Container>
   );
