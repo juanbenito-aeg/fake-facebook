22dbbc94bc791a40859e39833989e71b591291b2
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-21 14:20:01 +0200

    feat: create a component in Avatar.tsx that graphically indicates whether a user is online or not

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface AvatarProps {
   source: string;
+  online: boolean;
 }
 
 const Container = styled.View`
@@ -16,10 +17,22 @@ const ProfileImage = styled.Image`
   border-radius: 20px;
 `;
 
-const Avatar = ({ source }: AvatarProps) => {
+const UserActive = styled.View`
+  position: absolute;
+  bottom: -2px;
+  right: -2px;
+  width: 15px;
+  height: 15px;
+  border: 2px solid #ffffff;
+  border-radius: 7.5px;
+  background-color: #10d24b;
+`;
+
+const Avatar = ({ source, online }: AvatarProps) => {
   return (
     <Container>
       <ProfileImage source={source} />
+      {online && <UserActive />}
     </Container>
   );
 };
