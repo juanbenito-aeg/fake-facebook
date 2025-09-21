ff156056bdd7218b4e84ca3b3da4436d705f82e0
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-21 21:08:05 +0200

    feat: create a file with fake data for the stories slider

---
+++ data/fakeStories.ts
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    source: require("../assets/images/story5.jpg"),
+    user: require("../assets/images/28.jpg"),
+    name: "Frankie Stein",
+    checked: false,
+  },
+  {
+    source: require("../assets/images/story4.jpg"),
+    user: require("../assets/images/40.jpg"),
+    name: "Morty Jones",
+    checked: false,
+  },
+  {
+    source: require("../assets/images/story3.jpg"),
+    user: require("../assets/images/68.jpg"),
+    name: "King Kobra",
+    checked: true,
+  },
+  {
+    source: require("../assets/images/story2.jpg"),
+    user: require("../assets/images/8.jpg"),
+    name: "Turu Leta",
+    checked: false,
+  },
+];
